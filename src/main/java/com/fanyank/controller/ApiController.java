e813b427319e18bceb885389e1d1fc5e6efa3df7
Fish-Fan <fanyank@126.com>
2017-06-01 16:39:33 +0800

    使用requestBody接收payload请求

---
+++ src/main/java/com/fanyank/controller/ApiController.java
@@ -0,0 +1,44 @@
+package com.fanyank.controller;
+
+import com.fanyank.pojo.User;
+import com.fanyank.service.UserService;
+import com.google.common.collect.Maps;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * Created by yanfeng-mac on 2017/6/1.
+ */
+@Controller
+@RequestMapping(value = "/api")
+public class ApiController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/auth/login",produces = "text/html;charset=UTF-8")
+    @ResponseBody
+    public String userLogin(@RequestBody Map requestBody, HttpServletResponse response) {
+        String username = (String) requestBody.get("username");
+        String password = (String) requestBody.get("password");
+        System.out.println("username->" + username);
+        System.out.println("password->" + password);
+        User user = userService.findByUsername(username);
+//        Map<String, Object> result = Maps.newHashMap();
+        if (userService.isNull(user)) {
+//            result.put("state", "error");
+            return "false";
+        } else {
+//            result.put("state", "success");
+            return "true";
+        }
+
+
+    }
+
+}
